fb85d59fe9404d101a579fcdaef7c176b948e23d
SinghAyush07 <200604790+SinghAyush07@users.noreply.github.com>
2025-06-18 22:49:44 +0530

    18th June

---
+++ ARRAYS-ARRAYLIST/Sort0and1BTR.java
@@ -0,0 +1,25 @@
+// sort 0's and 1's using two pointer method 
+
+public class Sort0and1BTR {
+    public static void reverse(int[] arr, int i, int j) {
+        while(i<j){
+            if(arr[i] == 0) {
+                i++;
+            }
+            if(arr[j] == 1 ){
+                j--;
+            }
+            if(arr[i]==1 && arr[j]==0) {
+                arr[i] = 0;
+                arr[j] = 1;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,0,1,1,0,0,0,1,0,1,0};
+        reverse(arr,0,arr.length-1);
+        for(int ele : arr){
+            System.out.print(ele + " ");
+        }
+    }
+}
