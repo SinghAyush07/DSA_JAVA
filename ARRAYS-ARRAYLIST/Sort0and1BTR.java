1bd86b7aba267b5306f07bd0fa84686628c13e6b
SinghAyush07 <200604790+SinghAyush07@users.noreply.github.com>
2025-06-18 22:54:56 +0530

    18th June

--- ARRAYS-ARRAYLIST/Sort0and1BTR.java
+++ ARRAYS-ARRAYLIST/Sort0and1BTR.java
@@ -6,17 +6,17 @@ public class Sort0and1BTR {
             if(arr[i] == 0) {
                 i++;
             }
-            if(arr[j] == 1 ){
+            else if(arr[j] == 1 ){
                 j--;
             }
-            if(arr[i]==1 && arr[j]==0) {
+            else if(arr[i]==1 && arr[j]==0) {
                 arr[i] = 0;
                 arr[j] = 1;
             }
         }
     }
     public static void main(String[] args) {
-        int[] arr = {1,0,1,1,0,0,0,1,0,1,0};
+        int[] arr = {0,0,0,1,1,1};
         reverse(arr,0,arr.length-1);
         for(int ele : arr){
             System.out.print(ele + " ");
