a516abcf324679ab6fc4e65f42cf7d5553b26cbd
SinghAyush07 <200604790+SinghAyush07@users.noreply.github.com>
2025-06-19 11:40:35 +0530

    19th June

---
+++ ARRAYS-ARRAYLIST/DutchFlagAlgorithm.java
@@ -0,0 +1,17 @@
+// Sort the arry of 0's, 1's and 2's. (Dutch Flag Algoritm)
+
+public class DutchFlagAlgorithm {
+    public static void main(String[] args) {
+        int[] arr = {0,1,2,0,1,2,1,2,0,0};
+        int n = arr.length;
+
+        // we can use two pass algortihm
+        // count number of 0's and 1's
+        // put the counted number of 0's and 1's in
+        // the given array and then fill remaining spaces with 2's
+        
+        // But in interview tell the following algorithm :-
+        
+
+    }
+}
