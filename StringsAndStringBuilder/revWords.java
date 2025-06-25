20e7f1b0e7fe7fbc36249658f14500c518f0ade3
SinghAyush07 <200604790+SinghAyush07@users.noreply.github.com>
2025-06-25 09:22:22 +0530

    25th June

--- StringsAndStringBuilder/revWords.java
+++ StringsAndStringBuilder/revWords.java
@@ -1,7 +1,32 @@
 // Reverse each word in a given sentence. (Don't reverse the whole string)
 
+import java.util.Scanner;
+
 public class revWords {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Sentence : ");
+        String str = sc.nextLine();
+        str += " ";
+
+        // Defining a stringbuilder
+        StringBuilder sb = new StringBuilder(str.length());
+
+
+        // Method 1 : two pointer
+        int i = 0, j = 0;
+        while(i<str.length()) {
+            if(str.charAt(j) == ' ') {
+                StringBuilder s = new StringBuilder();
+                s.append(str.substring(i, j));
+                sb.append(s.reverse());
+                sb.append(" ");
+                i = j;
+                i++;
+            }
+            j++;
+        }
+        System.out.println(sb);
+        sc.close();
     }
 }
