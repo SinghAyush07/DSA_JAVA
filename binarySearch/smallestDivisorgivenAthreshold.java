1dc20404506a88f33b1fe1d9a4565debb981a1c3
SinghAyush07 <200604790+SinghAyush07@users.noreply.github.com>
2025-07-02 20:20:32 +0530

    2nd July

--- binarySearch/smallestDivisorgivenAthreshold.java
+++ binarySearch/smallestDivisorgivenAthreshold.java
@@ -2,30 +2,49 @@
 // Find the smallest Divisor given a Threshold
 
 public class smallestDivisorgivenAthreshold {
+    public static boolean isLess(int mid, int[] arr, int t) {
+        int sum=0;
+        boolean ans = false;
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i]%mid == 0)  sum += arr[i]%mid;
+            else sum += arr[i]%mid + 1;
+        }
+        if(sum<=t) ans = true;
+        return ans;
+    }
     public static void main(String[] args) {
-        int[] arr = {5,1,9,2};
+        int[] arr = {1,2,5,9};
         int t = 6; // threshold
-        // Brute force
         int n = arr.length;
-        int d; // divisor
-        int ans=0;
         int mx = Integer.MIN_VALUE;
-
-        // Method 1 : Brute Force the logic
         for(int i=0; i<n; i++) {
             mx = Math.max(mx, arr[i]);
         }
-        for(d=1; d<=mx; d++) {  
-            int sum = 0;
-            for(int i=0; i<n; i++) {
-                if(arr[i]%d == 0) sum += arr[i]/d;
-                else sum += arr[i]/d + 1;
-            }
-            if(sum <= t) {
-                ans = d;
-                break;
+        // Brute force
+        // Method 1 : Brute Force the logic
+        // for(d=1; d<=mx; d++) {  
+        //     int sum = 0;
+        //     for(int i=0; i<n; i++) {
+        //         if(arr[i]%d == 0) sum += arr[i]/d;
+        //         else sum += arr[i]/d + 1;       // add 1 if the element is not completely divisible by d.
+        //     }
+        //     if(sum <= t) {
+        //         ans = d;
+        //         break;
+        //     }
+
+        // using binary search method to find the d
+        int d=0;
+        int lo = 1, hi = mx;
+        while(lo<=hi) {
+            int mid = lo + (hi - lo)/2;
+            if(isLess(mid,arr,t)) {
+                hi = mid - 1;
+                d = mid;
+            } else {
+                lo = mid + 1;
             }
         }
-        System.out.println(ans);
+        System.out.println(d);
     }
 }
