4450d48590155c3b5e8308941a89c0c055395960
SinghAyush07 <200604790+SinghAyush07@users.noreply.github.com>
2025-07-04 23:02:30 +0530

    4th July

---
+++ Recursion/printNto1.java
@@ -0,0 +1,21 @@
+// Print n to 1.
+
+import java.util.Scanner;
+
+public class printNto1 {
+    public static void print(int n) {
+        if (n==0) {
+            return;
+        }
+        System.out.print(n + " ");
+        print(n-1);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the Number : ");
+        int n = sc.nextInt();
+        print(n);
+        sc.close();
+    }
+}
