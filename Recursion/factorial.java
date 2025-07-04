acc583d230c1eba4f913a1f11299c920c8f2749c
SinghAyush07 <200604790+SinghAyush07@users.noreply.github.com>
2025-07-04 13:50:20 +0530

    4th July

--- Recursion/factorial.java
+++ Recursion/factorial.java
@@ -3,17 +3,16 @@
 import java.util.Scanner;
 
 public class factorial {
-    public static int recursion(int n) {
+    public static int fact(int n) {
         if(n == 0) return 1;
-        return n*recursion(n-1);
-
+        return n*fact(n-1);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter the Number : ");
         int n = sc.nextInt();
-        int ans = recursion(n);
+        int ans = fact(n);
         System.out.println(ans);
         sc.close();
     }
