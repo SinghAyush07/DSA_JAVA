361d7faf2b2f488df50fe616fa00d2290b0e7b31
SinghAyush07 <200604790+SinghAyush07@users.noreply.github.com>
2025-07-08 18:20:35 +0530

    8th July

--- Recursion/fibonacci.java
+++ Recursion/fibonacci.java
@@ -6,12 +6,13 @@ public class fibonacci {
     public static int fibo(int n) {
         if(n<=1) return 1;
         return fibo(n-1) + fibo(n-2);
-    }
+    } 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter n : ");
         int n = sc.nextInt();
-        System.out.println(fibo(n));
+        System.out.println(fibo(n-1));
+        sc.close();
     }
 }
