6ffada870f027972b79d8cb44417cd7affeb88be
SinghAyush07 <200604790+SinghAyush07@users.noreply.github.com>
2025-07-12 14:24:50 +0530

    12th July

--- Recursion/PreInPost.java
+++ Recursion/PreInPost.java
@@ -3,12 +3,19 @@
 import java.util.Scanner;
 
 public class PreInPost {
-    public static int pip() {
-
+    public static void pip(int n) {
+        if(n == 0) return;
+        System.out.println(n);
+        pip(n-1);
+        System.out.println(n);
+        pip(n-1);
+        System.out.println(n);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        
+        System.out.print("Enter a number : ");
+        int n = sc.nextInt();
+        pip(n);
+        sc.close();
     }
 }
