235477dfbccd46402122caeca4097c9dcd0cafcf
SinghAyush07 <200604790+SinghAyush07@users.noreply.github.com>
2025-07-15 22:25:03 +0530

    15th July

--- Recursion/permutation.java
+++ Recursion/permutation.java
@@ -1,9 +1,21 @@
 // Finding all permutations of an string given all elements of the string are unique.
 
 public class permutation {
+    public static void printPermutations(String ans, String s) {
+        if(s.length() == 0) {
+            System.out.print(ans + " ");
+            return;
+        }
+        for(int i=0; i<s.length(); i++) {
+            char ch = s.charAt(i);
+            String left = s.substring(0, i);
+            String right = s.substring(i+1);
+            printPermutations(ans+ch, left+right);
+        }
+    }
     public static void main(String[] args) {
-        String s = "abc";
+        String s = "abcde";
 
-        printpermutation();
+        printPermutations("", s);
     }
 }
