d92a526e4ea83ed2b74fadbe0073e4644d9f6554
SinghAyush07 <200604790+SinghAyush07@users.noreply.github.com>
2025-07-23 17:46:20 +0530

    23rd July

--- MergeTwoSortedArray.java
+++ MergeTwoSortedArray.java
@@ -3,9 +3,7 @@
 // Time Complexity : O(m+n)
 public class MergeTwoSortedArray {
     public static void print(int[] arr) {
-        for(int ele : arr) {
-            System.out.print(ele + " ");
-        }
+        for(int ele : arr) System.out.print(ele + " ");
         System.out.println();
     }
     public static void merge(int[] a, int[] b, int[] c) {
