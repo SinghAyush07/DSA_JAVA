cf294bedfa7e895dfe94d8caaff9c8f76c47c61c
SinghAyush07 <200604790+SinghAyush07@users.noreply.github.com>
2025-09-07 11:11:11 +0530

    mergeSort

--- Advance_Sorting/mergeSort.java
+++ Advance_Sorting/mergeSort.java
@@ -3,48 +3,53 @@
 
 public class mergeSort {
     public static void print(int[] arr) {
-        for(int ele : arr) {
-            System.out.print(ele + " ");
+        for(int i=0; i<arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
     public static void merge(int[] a, int[] b, int[] c) {
-        int i = 0, j = 0, k = 0;
-        while(i < a.length && j < b.length) {
-            if(a[i] <= b[j]) c[k++] = a[i++];
-            else c[k++] = b[j++];
+        int m = a.length;
+        int n = b.length;
+        int i = 0, j = 0, k=0;
+        while(i<m && j<n) {
+            if(a[i] < b[j]) {c[k] = a[i]; i++;}
+            else {c[k] = b[j]; j++;}
+            k++;
         }
-        while(j<b.length) c[k++] = b[j++];
-        while(i<a.length) c[k++] = a[i++];
+        if(m<n) {
+            while(j<n) {
+                c[k] = b[j]; k++;j++;
+            }
+        } else {
+            while(i<m) {
+                c[k] = a[i]; k++; i++;
+            }
+        }
     }
     public static void mergesort(int[] arr) {
         int n = arr.length;
-        if (n==1) return;   // base case
-        // create two arrays of n/2 size each
+        if(n==1) return;
         int[] a = new int[n/2];
         int[] b = new int[n-n/2];
-        
         for(int i=0; i<n/2; i++) {
             a[i] = arr[i];
         }
-        for(int i=0; i< (n-n/2); i++) {
+        for(int i=0; i<n-n/2; i++) {
             b[i] = arr[n/2 + i];
         }
-        // magic
+        //magic
         mergesort(a);
         mergesort(b);
-
         // merge these 'a' and 'b'
         merge(a, b, arr);
     }
     public static void main(String[] args) {
-        int[] arr = {80,30,50,20,60,10,70,40};
-
-        System.out.print("Array : ");
+        int[] arr = {80, 10, 70, 30, 60, 40, 50, 20};
+        System.out.print("Origianl Array: ");
         print(arr);
-
         mergesort(arr);
-        System.out.print("Sorted Array : ");
+        System.out.print("Sorted Array: ");
         print(arr);
-    }    
+    }
 }
