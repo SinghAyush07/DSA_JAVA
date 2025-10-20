f487ceeafed44ae123a210e872512eb1ffc832ec
SinghAyush07 <200604790+SinghAyush07@users.noreply.github.com>
2025-10-20 17:26:21 +0530

    classes in JAVA

--- OOPS/src/OOPSinJAVA/StudentClass.java
+++ OOPS/src/OOPSinJAVA/StudentClass.java
@@ -1,28 +1,41 @@
 package OOPSinJAVA;
 
 public class StudentClass {
-    // creating a new data type -> multiple attribute
+    // creating a new data type with multiple attribute
     public static class Student {
         String name;
         int rollNo;
         double percent;
     }
 
-    public static class Car {
-        String name;
-        String type;
-        int price;
+    public static void print (Student s) {
+        System.out.println(s.name);
+        System.out.println(s.rollNo);
+        System.out.println(s.percent);
+    }
+    public static void changeName (Student s) {
+        s.name = "rahul";
     }
     public static void main(String[] args) {
+        // class Student {                  // class can also be declared within a function or a method
+        //     String name;
+        //     int rollNo;
+        //     double percent;
+        // }
+
         Student x, y;           // x and y are objects of Student Class
         x = new Student();      // declaration
         y = new Student();
         x.name = "Raghav";
         x.rollNo = 76;
         x.percent = 92.5;
-        System.out.println(x.rollNo + 8);
+        print(x);
+        changeName(x);          // same as array the value is passed by reference for a class instance
+        print(x);
         y.name = "Ayush";
         y.rollNo = 51;
         y.percent = 95.2;
+        //print(y);
+        
     }
 }
